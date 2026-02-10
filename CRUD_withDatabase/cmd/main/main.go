84189038c51470b08730c199eb1cb77672f42a4b
David Brush <brush.david99@gmail.com>
2026-02-10 08:23:50 -0600

    added project 3

---
+++ CRUD_withDatabase/cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/coldnebraska/golang/CRUD_withDatabase/pkg/routes"
+)
+
+func main() {
+	r := mux.newRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
