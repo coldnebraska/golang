46268e41750fa3b81cc67836a173e2b23d41eb74
David Brush <brush.david99@gmail.com>
2026-02-17 08:29:46 -0600

    fixed issue with mongodb not reading id

--- HRMS/main.go
+++ HRMS/main.go
@@ -22,7 +22,7 @@ const dbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017/" + dbName
 
 type Employee struct {
-	ID string `json:"id,omitempty" bson:"_id, omitempty"`
+	ID string `json:"id,omitempty" bson:"_id,omitempty"`
 	Name string `json:"name"`
 	Salary float64 `json:"salary"`
 	Age float64 `json:"age"`
